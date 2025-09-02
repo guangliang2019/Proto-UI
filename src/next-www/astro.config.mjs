57a8d590654d6fa426c9f78b3dee5a5e877979d0
guangliang2019 <guangliang2018@foxmail.com>
2025-09-02 17:02:17 +0800

    feat: 正在建设新文档站(59%)

--- src/next-www/astro.config.mjs
+++ src/next-www/astro.config.mjs
@@ -13,19 +13,27 @@ export default defineConfig({
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
       sidebar: [
         {
-          label: 'Guides',
+          label: '概要',
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
+            { label: '项目介绍', slug: 'overview/introduction' },
+            { label: '白皮书概要', slug: 'overview/whitepaper' },
+            { label: '原型库', slug: 'overview/prototypes' },
+            { label: '适配器库', slug: 'overview/adapters' },
+            { label: '时间线 & 里程碑', slug: 'overview/milestone' },
+            {
+              label: '贡献指引',
+              slug: 'overview/contribute',
+              badge: {
+                text: '征集中',
+                class: 'text-xs px-1.5 h-4.5 rounded-full bg-lime-400 text-black',
+              },
+            },
+            {
+              label: '安装 & 使用',
+              slug: 'overview/installation',
+              badge: { text: '计划中', class: 'text-xs px-2 h-5 rounded-full bg-zinc-800' },
+            },
           ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-        {
-          label: 'FAQ',
-          autogenerate: { directory: 'faq' },
         },
       ],
       components: {
@@ -38,7 +46,7 @@ export default defineConfig({
         TableOfContents: './src/components/override/TableOfContents/TableOfContents.astro',
         TwoColumnContent: './src/components/override/TwoColumnContent.astro',
         PageTitle: './src/components/override/PageTitle.astro',
-        MarkdownContent: './src/components/override/MarkdownContent.astro'
+        MarkdownContent: './src/components/override/MarkdownContent.astro',
       },
     }),
   ],
